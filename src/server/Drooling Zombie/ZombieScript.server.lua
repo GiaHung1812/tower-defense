006cedd2debd8a33d0b544cac41e4963ac162032
GiaHung <giahung18122004@gmail.com>
2026-08-19 15:15:09 +0700

    mob spawning and wave system

--- src/server/Drooling Zombie/ZombieScript.server.lua
+++
@@ -1,7 +0,0 @@
-local zombie = workspace:WaitForChild("Drooling Zombie")
-local waypoints = workspace.Waypoitns
-
-for waypoint = 1, #waypoints:GetChildren() do
-	zombie.Humanoid:MoveTo(waypoints[waypoint].Position)
-	zombie.Humanoid.MoveToFinished:Wait()
-end
--- src/server/Modules/Mob.lua
+++ src/server/Modules/Mob.lua
@@ -1,7 +1,40 @@
+local ServerStorage = game:GetService("ServerStorage")
+
 local mob = {}
 
-function mob.Spawn(name, map)
-	print(name, map)
+function mob.Move(mob, map)
+	local humanoid = mob:WaitForChild("Humanoid")
+	local waypoints = map.Waypoints:GetChildren()
+
+	table.sort(waypoints, function(a, b)
+		return tonumber(a.Name) < tonumber(b.Name)
+	end)
+
+	for _, waypoint in ipairs(waypoints) do
+		humanoid:MoveTo(waypoint.Position)
+		humanoid.MoveToFinished:Wait()
+	end
+
+	mob:Destroy()
+end
+
+function mob.Spawn(name, quantity, map)
+	local mobExists = ServerStorage.Mobs:FindFirstChild(name)
+
+	if mobExists then
+		for i = 1, quantity do
+			task.wait(0.5)
+			local newMob = mobExists:Clone()
+			newMob.HumanoidRootPart.CFrame = map.Start.CFrame
+			newMob.Parent = map.Mob
+
+			task.spawn(function()
+				mob.Move(newMob, map)
+			end)
+		end
+	else
+		warn("Requested mob does not exist:", name)
+	end
 end
 
 return mob
